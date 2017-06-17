6b3b9090081c536c1ec5652f94067f3380c6d289
Anderson Fraga <anderson.fraga@kinghost.com.br>
2017-06-17 15:01:32 -0300

    Inicio read

--- filesystem.c
+++ filesystem.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/wait.h>
 #include "libdisksimul.h"
 #include "filesystem.h"
 
 /**
  * @brief Format disk.
- * 
+ *
  */
 
 /*
@@ -78,8 +79,9 @@ look:for (int i = 0; i < entries_length; i++) {
 					if (new) {
 						printf("Cannot create '%s': File exists\n", pch);
 						return 0;
-					} else
+					} else {
 						return entrada;
+					}
 				} else
 					if (entrada->dir) {
 						*sector_pointer = entrada->sector_start;
@@ -100,7 +102,7 @@ look:for (int i = 0; i < entries_length; i++) {
 					if(strlen(pch) > 20) {
 						printf("Cannot create '%s': File name too long\n", pch);
 						return 0;
-					}	
+					}
 					strcpy(entrada->name, pch);
 					return entrada;
 				} else {
@@ -121,20 +123,20 @@ int fs_format(){
 	int ret, i;
 	struct root_table_directory root_dir;
 	struct sector_data sector;
-	
+
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 1)) != 0 ){
 		return ret;
 	}
-	
+
 	memset(&root_dir, 0, sizeof(root_dir));
-	
+
 	root_dir.free_sectors_list = 1; /* first free sector. */
-	
+
 	ds_write_sector(0, (void*)&root_dir, SECTOR_SIZE);
-	
+
 	/* Create a list of free sectors. */
 	memset(&sector, 0, sizeof(sector));
-	
+
 	for(i=1;i<NUMBER_OF_SECTORS;i++){
 		if(i<NUMBER_OF_SECTORS-1){
 			sector.next_sector = i+1;
@@ -143,11 +145,11 @@ int fs_format(){
 		}
 		ds_write_sector(i, (void*)&sector, SECTOR_SIZE);
 	}
-	
+
 	ds_stop();
-	
+
 	printf("Disk size %d kbytes, %d sectors.\n", (SECTOR_SIZE*NUMBER_OF_SECTORS)/1024, NUMBER_OF_SECTORS);
-	
+
 	return 0;
 }
 
@@ -159,7 +161,7 @@ int fs_format(){
  */
 int fs_create(char* input_file, char* simul_file){
 	int ret;
-	
+
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 0)) != 0 ){
 		return ret;
 	}
@@ -213,7 +215,7 @@ int fs_create(char* input_file, char* simul_file){
 		char buffer[sector_size];
 
 		fseek(fp, SEEK_SET, position_inside_file);
-		fread(buffer, sector_size, 1, fp);
+		fread(buffer, 1, sector_size, fp);
 
 		ds_read_sector(sector_pointer, (void*)&sector, SECTOR_SIZE);
 
@@ -228,16 +230,16 @@ int fs_create(char* input_file, char* simul_file){
 	}
 
 	root_dir.free_sectors_list = sector_pointer;
-	
+
 	ds_write_sector(0, (void*)&root_dir, SECTOR_SIZE);
-	
+
 	if (td_sector)
 		ds_write_sector(td_sector, (void*)&td, SECTOR_SIZE);
 
 	//print_sectors();
 
 	ds_stop();
-	
+
 	return 0;
 }
 
@@ -252,11 +254,51 @@ int fs_read(char* output_file, char* simul_file){
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 0)) != 0 ){
 		return ret;
 	}
-	
-	/* Write the code to read a file from the simulated filesystem. */
-	
+
+	if (access(output_file, W_OK) != -1 ) {
+		printf("File %s already exists\n", output_file);
+		return 1;
+	}
+
+	FILE *fp = fopen(output_file, "w+");
+
+	struct root_table_directory root_dir;
+	ds_read_sector(0, (void*)&root_dir, SECTOR_SIZE);
+
+	struct table_directory td;
+	int td_sector;
+	struct file_dir_entry *entrada = get_file_dir_entry(&root_dir, simul_file, 0, &td, &td_sector);
+
+	if (!entrada) {
+		printf("Simulated file %s not found\n", simul_file);
+		return 1;
+	}
+
+	int sector_pointer = entrada->sector_start;
+	struct sector_data sector;
+	int size = entrada->size_bytes;
+	int pos_init = 0;
+
+	while(1) {
+		int sector_size = (size > SECTOR_SIZE - sizeof(int) ? SECTOR_SIZE - sizeof(int) : size);
+
+		ds_read_sector(sector_pointer, (void*) &sector, sector_size);
+		fwrite(sector.data, 1, sector_size, fp);
+
+		size -= sector_size;
+
+		if (!size)
+			break;
+
+		sector_pointer = sector.next_sector;
+	}
+
+	fclose(fp);
+
+	//print_sectors();
+
 	ds_stop();
-	
+
 	return 0;
 }
 
@@ -272,7 +314,7 @@ int fs_del(char* simul_file){
 	}
 
 	//print_sectors();
-	
+
 	struct root_table_directory root_dir;
 	ds_read_sector(0, (void*)&root_dir, SECTOR_SIZE);
 
@@ -307,7 +349,7 @@ int fs_del(char* simul_file){
 	strcpy(entrada->name, "");
 
 	ds_write_sector(0, (void*)&root_dir, SECTOR_SIZE);
-	
+
 	if (td_sector)
 		ds_write_sector(td_sector, (void*)&td, SECTOR_SIZE);
 
@@ -326,7 +368,7 @@ int fs_ls(char *dir_path){
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 0)) != 0 ){
 		return ret;
 	}
-	
+
 	int td_sector = 0;
 	struct root_table_directory root_dir;
 	struct table_directory td;
@@ -364,9 +406,9 @@ int fs_ls(char *dir_path){
 		}
 	printf("└───┴──────────────────────┴─────────┘\n");
 	printf("\nQuantity: %d Total size: %d kB\n", file_count, total_size/1024);
-	
+
 	ds_stop();
-	
+
 	return 0;
 }
 
@@ -377,7 +419,7 @@ int fs_ls(char *dir_path){
  */
 int fs_mkdir(char* directory_path){
 	int ret;
-	
+
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 0)) != 0 ){
 		return ret;
 	}
@@ -399,28 +441,28 @@ int fs_mkdir(char* directory_path){
 	entrada->sector_start = root_dir.free_sectors_list;
 
 	sector_pointer = root_dir.free_sectors_list;
-	
+
 	struct sector_data sector;
 
 	ds_read_sector(sector_pointer, (void*)&sector, SECTOR_SIZE);
 
 	struct table_directory *buffer = malloc(sizeof (struct table_directory));
-	
+
 	root_dir.free_sectors_list = sector.next_sector;
-	
+
 	memcpy(&sector, buffer, SECTOR_SIZE);
 
 	ds_write_sector(sector_pointer, (void*)&sector, SECTOR_SIZE);
-	
+
 	ds_write_sector(0, (void*)&root_dir, SECTOR_SIZE);
 
 	if(td_sector)
 		ds_write_sector(td_sector,(void*)&td,SECTOR_SIZE);
 
 	ds_stop();
-	
+
 	return 0;
-}	
+}
 
 /**
  * @brief Remove directory from the simulated filesystem.
@@ -432,16 +474,16 @@ int fs_rmdir(char *directory_path){
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 0)) != 0 ){
 		return ret;
 	}
-	
+
 	/* Write the code to delete a directory. */
-	
+
 	ds_stop();
-	
+
 	return 0;
 }
 
 /**
- * @brief Generate a map of used/available sectors. 
+ * @brief Generate a map of used/available sectors.
  * @param log_f Log file with the sector map.
  * @return 0 on success.
  */
@@ -458,27 +500,27 @@ int fs_free_map(char *log_f){
 	if ( (ret = ds_init(FILENAME, SECTOR_SIZE, NUMBER_OF_SECTORS, 0)) != 0 ){
 		return ret;
 	}
-	
+
 	/* each byte represents a sector. */
 	sector_array = (char*)malloc(NUMBER_OF_SECTORS);
 
 	/* set 0 to all sectors. Zero means that the sector is used. */
 	memset(sector_array, 0, NUMBER_OF_SECTORS);
-	
+
 	/* Read the root dir to get the free blocks list. */
 	ds_read_sector(0, (void*)&root_dir, SECTOR_SIZE);
-	
+
 	next = root_dir.free_sectors_list;
 
 	while(next){
 		/* The sector is in the free list, mark with 1. */
 		sector_array[next] = 1;
-		
+
 		/* move to the next free sector. */
 		ds_read_sector(next, (void*)&sector, SECTOR_SIZE);
-		
+
 		next = sector.next_sector;
-		
+
 		free_space += SECTOR_SIZE;
 	}
 
@@ -489,29 +531,29 @@ int fs_free_map(char *log_f){
 		ds_stop();
 		return 1;
 	}
-	
+
 	/* Write the the sector map to the log file. */
 	for(i=0;i<NUMBER_OF_SECTORS;i++){
 		if(i%32==0) fprintf(log, "%s", "\n");
 		fprintf(log, " %d", sector_array[i]);
 	}
-	
+
 	fclose(log);
-	
+
 	/* Execute gnuplot to generate the sector's free map. */
 	pid = fork();
 	if(pid==0){
 		execvp("gnuplot", exec_params);
 		return 0;
 	}
-	
+
 	wait(&status);
-	
+
 	free(sector_array);
-	
+
 	ds_stop();
-	
+
 	printf("Free space %d kbytes.\n", free_space/1024);
-	
+
 	return 0;
 }
--- fs_simul.c
+++ fs_simul.c
@@ -15,28 +15,30 @@ void usage(char *exec){
 
 
 int main(int argc, char **argv){
-	
+
 	if(argc<2){
 		usage(argv[0]);
 	}else{
-	
+
 		/* Disk formating. */
 		if( !strcmp(argv[1], "-format"))
 			fs_format();
 		else if( !strcmp(argv[1], "-create"))
 			fs_create(argv[2], argv[3]);
+		else if ( !strcmp(argv[1], "-read"))
+			fs_read(argv[2], argv[3]);
 		else if( !strcmp(argv[1], "-ls"))
 			fs_ls(argv[2]);
 		else if( !strcmp(argv[1], "-del"))
 			fs_del(argv[2]);
 		else if ( !strcmp(argv[1], "-mkdir"))
 			fs_mkdir(argv[2]);
-		
+
 	}
 
 	/* Create a map of used/free disk sectors. */
 	fs_free_map("log.dat");
-	
-	return 0; 
+
+	return 0;
 }
-	
+
