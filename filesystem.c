9e6be82138f74eee26babb144b5084b6d6ded689
Eduardo Hoefel <eduardo.hoefel@acad.pucrs.br>
2017-06-19 22:04:50 -0300

    - Ajuste de bugs com arquivos com 20 caracteres; Limpeza de código;

--- filesystem.c
+++ filesystem.c
@@ -11,55 +11,6 @@
  *
  */
 
-/*
-int print_sectors() {
-	struct root_table_directory root_dir;
-	ds_read_sector(0, (void*)&root_dir, SECTOR_SIZE);
-	struct sector_data sector;
-	int sector_pointer = root_dir.free_sectors_list;
-	printf("0");
-	while(sector_pointer) {
-		printf(" -> %d", sector_pointer);
-		ds_read_sector(sector_pointer, (void*)&sector, SECTOR_SIZE);
-		sector_pointer = sector.next_sector;
-		//if (sector_pointer > 10)
-			//break;
-	}
-	printf("\n");
-	return 0;
-
-}
-
-void debug_sector(int pos) {
-	if (!pos) {
-		struct root_table_directory root_dir;
-		ds_read_sector(0, (void*)&root_dir, SECTOR_SIZE);
-		printf("free_sectors_list: %d\n", root_dir.free_sectors_list);
-		printf("entries:\n");
-			printf("┌───┬──────────────────────┬─────────┬───────┐\n");
-			printf("│ T │ Nome                 │ Tamanho │ Setor │\n");
-			printf("├───┼──────────────────────┼─────────┼───────┤\n");
-		for (int i = 0; i < 15; i++) {
-			struct file_dir_entry *entrada = &root_dir.entries[i];
-			printf("│ %c │ %.20s%*s│ %4d kB │  %.4x │\n", (entrada->dir? 'd' : 'f'), entrada->name, (int) (21 - strlen(entrada->name)), " ", entrada->size_bytes/1024, entrada->sector_start);
-
-		}
-			printf("└───┴──────────────────────┴─────────┴───────┘\n");
-		printf("\n");
-
-	} else {
-		unsigned char buffer[16][SECTOR_SIZE/16];
-		ds_read_sector(pos, (void*)&buffer, SECTOR_SIZE);
-		for (int j = 0; j < 16; j++) {
-			for (int i = 0; i < SECTOR_SIZE/16; i++)
-	   			printf("%.2x ", buffer[j][i]);
-			printf("\n");
-		}
-	}
-
-}
-*/
-
 struct file_dir_entry* get_file_dir_entry(struct root_table_directory* root_dir, char* path, int new, struct table_directory* td, int* sector_pointer) {
 	*sector_pointer = 0;
 	if (!strlen(path) || !strcmp(path, "/"))
@@ -74,14 +25,17 @@ look:for (int i = 0; i < entries_length; i++) {
 		entrada = &entries[i];
 		if (entrada->sector_start) {
 			//entrada->name[20] = '\0';
-			if (!strcmp(entrada->name, pch)) {
+			if (!strncmp(entrada->name, pch, 20)) {
 				if (next_pch == NULL) {
 					if (new) {
 						printf("Cannot create '%s': File exists\n", pch);
-						return 0;
-					} else {
-						return entrada;
+						return NULL;
 					}
+					if (strlen(pch) > 20) {
+						printf("File name too long\n");
+						return NULL;
+					}
+					return entrada;
 				} else
 					if (entrada->dir) {
 						*sector_pointer = entrada->sector_start;
@@ -93,7 +47,7 @@ look:for (int i = 0; i < entries_length; i++) {
 						goto look;
 					} else {
 						printf("Cannot access '%s': No such file or directory\n", pch);
-						return 0;
+						return NULL;
 					}
 			}
 		} else
@@ -101,14 +55,13 @@ look:for (int i = 0; i < entries_length; i++) {
 				if (next_pch == NULL) {
 					if(strlen(pch) > 20) {
 						printf("Cannot create '%s': File name too long\n", pch);
-						return 0;
+						return NULL;
 					}
 					strcpy(entrada->name, pch);
 					return entrada;
-				} else {
-					printf("Cannot access '%s': No such file or directory\n", pch);
-					return 0;
 				}
+				printf("Cannot access '%s': No such file or directory\n", pch);
+				return NULL;
 			}
 }
 	if (new)
@@ -116,7 +69,7 @@ look:for (int i = 0; i < entries_length; i++) {
 	else
 		printf("No such file or directory\n");
 
-	return 0;
+	return NULL;
 }
 
 int fs_format(){
@@ -277,7 +230,6 @@ int fs_read(char* output_file, char* simul_file){
 	int sector_pointer = entrada->sector_start;
 	struct sector_data sector;
 	int size = entrada->size_bytes;
-	int pos_init = 0;
 
 	while(1) {
 		int sector_size = (size > SECTOR_SIZE - sizeof(int) ? SECTOR_SIZE - sizeof(int) : size);
@@ -323,6 +275,11 @@ int fs_del(char* simul_file){
 	struct file_dir_entry *entrada = get_file_dir_entry(&root_dir, simul_file, 0, &td, &td_sector);
 	if (!entrada)
 		return 1;
+
+	if (entrada->dir) {
+		printf("File cannot be a directory\n");
+		return 1;
+	}
 
 	int sector_pointer = entrada->sector_start;
 	int first_pointer = sector_pointer;
@@ -392,19 +349,22 @@ int fs_ls(char *dir_path){
 
 	int file_count = 0;
 	int total_size = 0;
+	char name[21];
 
-	printf("┌───┬──────────────────────┬─────────┐\n");
-	printf("│ T │ Name                 │  Size   │\n");
-	printf("├───┼──────────────────────┼─────────┤\n");
+	printf("┌───┬───────────────────────┬─────────┐\n");
+	printf("│ T │ Name                  │  Size   │\n");
+	printf("├───┼───────────────────────┼─────────┤\n");
 	for (int i = 0; i < size; i++)
 		if (entries[i].sector_start) {
 			entrada = &entries[i];
 			file_count++;
 			total_size += entrada->size_bytes;
-			printf("│ %c │ %.20s%*s│ %4d kB │\n", (entrada->dir? 'd' : 'f'), entrada->name, (int) (21 - strlen(entrada->name)), " ", entrada->size_bytes/1024);
+			strcpy(name, entrada->name);
+			name[20] = '\0';
+			printf("│ %c │ %.21s%*s│ %4d kB │\n", (entrada->dir? 'd' : 'f'), name, (int) (22 - strlen(name)), " ", entrada->size_bytes/1024);
 
 		}
-	printf("└───┴──────────────────────┴─────────┘\n");
+	printf("└───┴───────────────────────┴─────────┘\n");
 	printf("\nQuantity: %d Total size: %d kB\n", file_count, total_size/1024);
 
 	ds_stop();
